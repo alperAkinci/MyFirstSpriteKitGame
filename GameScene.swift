70e20cbd679e4b78177058b51d4bd4b8f81280cb
AlperAkinci <alperakinci1@gmail.com>
2016-02-29 15:43:42 +0200

    Ninja throws projectile ,action added

--- GameScene.swift
+++ GameScene.swift
@@ -38,7 +38,7 @@ extension CGPoint{
         return sqrt((x*x) + (y*y))
     }
     
-    
+    //NOTE: The normalized vector of CGPoint vector is a vector in the same direction but with norm (length) 1.
     func normalized() -> CGPoint {
         //Operator Overloading in Swift (/)
         return self / length()
@@ -93,8 +93,47 @@ class GameScene: SKScene {
         
     }
     
-   
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        //1 - Choose the touch to work with
+        
+        guard let touch = touches.first else{
+            
+            return /* return will end the func immediately , no further code inside this method will be executed*/
+        }
+        let touchLocation = touch.locationInNode(self)
+        
+        //2 - Set up initial location of projectile(the power that ninja thrown )
+        let projectile = SKSpriteNode(imageNamed: "projectile")
+        projectile.position = player.position
+    
+        //3 - Determine offset of location to projectile
+        let offset = touchLocation - projectile.position
+        
+        //4 - you cant shoot backward
+        if (offset.x)<0 { return }
+        
+        //5 - Already checked the position so add projectile
+        addChild(projectile)
+        
+        //6 - Get the direction of where you shoot 
+        let direction = offset.normalized()
+        
+        //7 - Make it shoot far enough to be guaranteed off screen 
+            //It will definitely be long enough to go past the edge of the screen
+        let shootAmount = direction * 1000
+        
+        //8 - Add the shoot amount to the current position 
+        let realDest = shootAmount + projectile.position
+        
+        //9 - Create the actions 
+        let actionMove = SKAction.moveTo(realDest, duration: 2.0)
+        let actionMoveDone = SKAction.removeFromParent()
+        
+        projectile.runAction(SKAction.sequence([actionMove,actionMoveDone]))
+    
+    }
 
+    
     
     override func didMoveToView(view: SKView) {
         //Background Color of Scene
